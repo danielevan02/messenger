b25e572efdc8ee5ddd910f213ada47ae7b7c52e2
Eric GabaG <eric.gabag@gmail.com>
2024-11-30 13:53:53 +0700

    fixing auth connection

--- app/api/pusher/auth/route.ts
+++ app/api/pusher/auth/route.ts
@@ -1,27 +1,44 @@
-import { getServerSession } from "next-auth";
+import { pusherServer } from '@/app/lib/pusher'; 
+import { getServerSession } from 'next-auth';
 
-import { pusherServer } from "@/app/lib/pusher";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { NextResponse } from "next/server";
-
-export default async function handler(
-  request: Request
-) {
-  const session = await getServerSession(authOptions);
-
-  if (!session?.user?.email) {
-    return new NextResponse('Internal Error', {status: 500})
-  }
-
-  const body = await request.json()
-
-  const socketId = body.socket_id
-  const channel = body.channel_name
-  const data = {
-    user_id: session.user.email
-  };
-
-  const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
-
-  return NextResponse.json(authResponse);
+async function streamToString(stream: any) {
+    const chunks = [];
+    for await (const chunk of stream) {
+        chunks.push(chunk);
+    }
+    return Buffer.concat(chunks).toString('utf8');
 }
+
+const handler = async (req: Request, res: Response) => {
+    try {
+
+        const session = await getServerSession()
+
+        const bodyAsString = await streamToString(req.body)
+        const body = {
+            'socket_id': bodyAsString.split('&')[0].split('=')[1],
+            'channel_name': `presence-mesengger`
+        }
+
+        if (!session?.user?.email) {
+            return Response.json({ 'error': 'You have to be authenticated' }, { status: 401 })
+        }
+
+        const socketId = body.socket_id;
+        const channel = body.channel_name;
+        const data = {
+            user_id: session.user.email,
+        }
+
+        console.log(data)
+
+        const authResponse = pusherServer.authorizeChannel(socketId, channel, data)
+
+        return Response.json(authResponse)
+    } catch (err) {
+        console.error(err)
+        return Response.json({ 'error': 'internal error' }, { status: 500 })
+    }
+}
+
+export { handler as POST }
